23acef10c8acfba77d050bb37a90918b0b41f409
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-03-07 10:30:41 +0530

    using functional way of recursion

---
+++ factorial2.cpp
@@ -0,0 +1,22 @@
+// functional way
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n ";
+    cin>>n;
+    cout << factorial(n);
+
+    return 0;
+}
