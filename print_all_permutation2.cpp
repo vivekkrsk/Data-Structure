a1d619da39d4b90746449f738590a8bc206139a5
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-03-14 05:41:56 +0530

    using recursion

---
+++ print_all_permutation2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+   void recurepermute(int index, vector<int>&nums, vector<vector<int>> &ans)
+   {
+       if(index==nums.size())
+       {
+           ans.push_back(nums);
+           return;
+       }
+       for(int i=index; i<nums.size(); i++)
+       {
+           swap(nums[index], nums[i]);
+           recurepermute(index+1,nums,ans);
+           swap(nums[index],nums[i]);
+       }
+   }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds; 
+        recurepermute(0,nums,ans);
+        return ans;
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
