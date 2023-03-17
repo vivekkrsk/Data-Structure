53311b155faaac7a05d4ad0010db0cb6523277f2
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-03-17 07:12:44 +0530

    Using DP

---
+++ fibbonaci_number_tab.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int prev2=0;
+    int prev=1;
+    for (int i = 2; i <= n; i++)
+    {
+        int curi=prev+prev2;
+        prev2=prev;
+        prev=curi;
+    }
+    cout<<prev;
+    
+
+    return 0;
+}
