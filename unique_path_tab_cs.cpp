4576e665a42311e4db60ff09fd900decd41b2157
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-03-30 07:55:23 +0530

    using dp

---
+++ unique_path_tab_cs.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int uniquePaths(int m, int n) {
+   int dp[m][n];
+   for(int i= 0; i<m; i++)
+   {
+       for(int j = 0; j<n; j++)
+       {
+           if(i==0 && j==0)
+           {
+               dp[i][j]=1;
+           }
+           else{
+               int up =0;
+               int left =0;
+               if (i > 0) {
+                 up = dp[i-1][j];
+               }
+               if (j > 0) {
+                 left = dp[i][j-1];
+               }
+               dp[i][j]= up+left;
+           }
+       }
+   }
+
+   return dp[m-1][n-1];
+   
+	// Write your code here.
+}
+
+
+int main()
+{
+    
+
+    return 0;
+}
