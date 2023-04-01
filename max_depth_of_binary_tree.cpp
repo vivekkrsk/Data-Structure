776627104b6f328fca7ff6260af6475c7faf679e
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-04-01 07:50:28 +0530

    time complexity O(n)

---
+++ max_depth_of_binary_tree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int lh = maxDepth(root-> left);
+        int rh = maxDepth(root->right);
+        
+        return 1+ max(lh,rh);
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
