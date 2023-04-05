961a8353a53f60b879acd3dbd774b3fdab22cc5d
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-04-05 06:48:45 +0530

    binary tree

---
+++ check_for_symmetry_of_bt.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return root==NULL || isSymetricHelp(root->left, root->right);
+    }
+
+    bool isSymetricHelp(TreeNode* left, TreeNode* right)
+    {
+        if(left==NULL || right==NULL) return left==right;
+
+        if(left->val!= right->val) return false;
+
+        return isSymetricHelp(left->left, right->right) && isSymetricHelp(left->right, right->left);
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
