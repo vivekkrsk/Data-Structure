cdcc0da0410f743c11b4ef315e2d56c381a8d026
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-04-15 17:03:47 +0530

    binary search tree

---
+++ contruct_bst_from_preorder.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* build(vector<int>& A, int & i, int bound)
+    {
+        if(i==A.size() || A[i]>bound) return NULL;
+        TreeNode* root = new TreeNode(A[i++]);
+        root->left = build(A,i,root->val);
+        root->right = build(A,i,bound);
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build(preorder,i,INT_MAX);
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
