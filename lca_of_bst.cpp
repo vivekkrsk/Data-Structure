d2f5c5486a02c5dc2a2398d5f62cc2ac302f9802
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-04-15 17:03:05 +0530

    binary search tree

---
+++ lca_of_bst.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+
+        int cur= root->val;
+        if(cur<p->val && cur<q->val) return lowestCommonAncestor(root->right, p, q);
+        if(cur>p->val && cur>q->val) return lowestCommonAncestor(root->left, p, q);
+        return root;
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
