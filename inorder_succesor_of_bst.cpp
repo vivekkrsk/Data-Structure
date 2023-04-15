8453242985ca6d6e8307beda8f274dde4523a637
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-04-15 17:04:11 +0530

    binary search tree

---
+++ inorder_succesor_of_bst.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        TreeNode* successor = NULL;
+        
+        while (root != NULL) {
+            
+            if (p->val >= root->val) {
+                root = root->right;
+            } else {
+                successor = root;
+                root = root->left;
+            }
+        }
+        
+        return successor;
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
